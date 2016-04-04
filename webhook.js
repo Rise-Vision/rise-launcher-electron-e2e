f734f1be8fbb43fe23c0fda0711a5fa78da00aa8
Tyler Johnson <mail@tyler-johnson.ca>
2016-04-04 12:27:23 -0400

    Add busy handling
    The webhook only runs one e2e test at a time due to os level side
    effects that would otherwise potentially interfere with one another.

--- webhook.js
+++ webhook.js
@@ -2,14 +2,24 @@ const route = require("koa-route"),
 path = require("path"),
 koa = require("koa")();
 
+var isBusy = false;
+
 koa.on("error", (err)=>{
   log.debug(`server error: ${err.message}`);
+  isBusy = false;
+});
+
+koa.use(function* busy(next) {
+  if (isBusy) {return this.throw(503);}
+  isBusy = true;
+  this.req.on("close", ()=>{isBusy = false});
+  yield next;
 });
 
 koa.use(function* initialFailingStatus(next) {
   log.debug(`Request for ${this.url}`);
-  this.status = 503;
-  this.body = 503;
+  this.status = 500;
+  this.body = 500;
   yield next;
 });
 
@@ -17,7 +27,8 @@ koa.use(route.get("/install-and-upgrade/:version", require("./e2e-tests/install-
 
 module.exports = {
   listen() {
-    koa.listen(9950).timeout = 0;
-    log.debug("listening on 9950");
+    var port = 9950;
+    koa.listen(port).timeout = 0;
+    log.debug(`listening on ${port}`);
   }
 };
