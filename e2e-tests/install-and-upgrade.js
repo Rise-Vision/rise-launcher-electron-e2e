0fc35d24a9c4d5774b2fbe17c3de33740e9fc0b1
Tyler Johnson <mail@tyler-johnson.ca>
2016-04-08 15:44:33 -0400

    Improve error handling in downloader

--- e2e-tests/install-and-upgrade.js
+++ e2e-tests/install-and-upgrade.js
@@ -10,7 +10,10 @@ module.exports = function*(version) {
   log.debug(`Using bundle version ${version}`);
   yield cleanPrevious;
 
-  yield downloader.downloadInstaller(version, this).catch((err)=>{this.throw(err);});
+  yield downloader.downloadInstaller(version, this).catch((err)=>{
+    log.debug("download error " + require("util").inspect(err));
+    this.throw(500);
+  });
 
   yield platform.setFilePermissions(downloader.getDownloadedInstallerFilePath(), 0755);
 
