c6e7e3f7b6f448658d6ef51c6db07159ddec0c55
santiagonoguez <santiago@nul-unu.com>
2018-11-13 11:00:32 -0600

    add script that runs e2e test on previously downloaded installer and expected images

---
+++ test-display-runner-using-downloaded-installer.js
@@ -0,0 +1,29 @@
+global.log = console;
+const displayId = process.argv[2];
+const numberOfPrints = process.argv[3];
+
+const presentation = require("./presentation");
+const installerStarter = require("./installer-starter");
+
+const testDisplay = function () {
+  const ctx = {timeouts: {presentation: null}};
+  console.log(`Argument: ${displayId}`);
+
+  Promise.resolve().then(()=>{
+    installerStarter.startDownloadedInstaller();
+    presentation.confirmPresentationVisibility(ctx, "jpg", numberOfPrints)
+    .then(()=>{
+      console.log("Success")
+      process.exit();
+    })
+    .catch((err)=>{
+      console.log("Test error");
+      process.exit(1);
+    });
+  })
+  .catch((err)=>{
+    console.log("installer start error " + require("util").inspect(err));
+    process.exit(1);
+  });
+}
+testDisplay();
